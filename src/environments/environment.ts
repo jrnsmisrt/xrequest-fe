141e6c9093e22d02b30838db036dd8000906bde2
jrnsmisrt <jeroen.smissaert@gmail.com>
2023-08-07 11:47:03 +0200

    Create project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,14 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+const localBackend = 'http://localhost:8081';
+const localFrontend = 'http://localhost:4200';
+
 export const environment = {
-  production: false
+  production: false,
+  localBackend: localBackend,
+  localFrontend: localFrontend
 };
 
 /*
