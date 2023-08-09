8910d2b5ba464f12079bdb2c8e97cd7a7946ede5
jeroen <jeroen.smissaert@gmail.com>
2023-08-09 10:32:40 +0200

    update app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,22 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpService} from "../service/http.service";
-import {map, Observable, of, take} from "rxjs";
-import {XRequest} from "../interface/XRequest";
-import {Author} from "../interface/author";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    title = 'XRequests ~ enjoy life - do good';
-    navStatus = false;
-    mobileNav = true;
+  title = 'XRequests ~ enjoy life - do good';
+  navStatus = false;
+  mobileNav = true;
 
-    constructor(private httpService: HttpService) {
-    }
+  constructor() {
+  }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
-    toggleNav() {
-        this.navStatus = !this.navStatus;
-    }
+  toggleNav() {
+    this.navStatus = !this.navStatus;
+  }
 }
