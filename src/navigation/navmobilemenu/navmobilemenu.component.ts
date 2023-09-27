797fd2fea49c37cbf45a4416487af5fcd998fa6b
jrnsmisrt <jeroen.smissaert@gmail.com>
2023-09-27 10:39:12 +0200

    improve navmobile accordion

--- src/navigation/navmobilemenu/navmobilemenu.component.ts
+++ src/navigation/navmobilemenu/navmobilemenu.component.ts
@@ -2,13 +2,18 @@ import {Component, Input} from '@angular/core';
 import {MenuItem} from "../../interface/menu-item";
 
 @Component({
-    selector: 'app-navmobilemenu',
-    templateUrl: './navmobilemenu.component.html',
-    styleUrls: ['./navmobilemenu.component.css']
+  selector: 'app-navmobilemenu',
+  templateUrl: './navmobilemenu.component.html',
+  styleUrls: ['./navmobilemenu.component.css']
 })
 export class NavmobilemenuComponent {
+  toggled: { id: number, status: boolean } = {id: 0, status: true};
 
-    @Input() navStatus = false;
-    @Input() mobileNavCol = '';
-    @Input() menuItemList: MenuItem[] = [];
+  @Input() navStatus = false;
+  @Input() mobileNavCol = '';
+  @Input() menuItemList: MenuItem[] = [];
+
+  toggleAccordion(i: number, toggle: boolean) {
+    this.toggled = {id: i, status: !toggle}
+  }
 }
